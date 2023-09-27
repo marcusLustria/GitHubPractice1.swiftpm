0153a3ac0a1d9576b85ee54ed960bf98cffbd750
Marcus Lustria <mlustria6238@stu.d214.org>
2023-09-27 15:18:51 -0500

    I created the action for the button.

--- HeaderView.swift
+++ HeaderView.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct HeaderView {
+    @Binding var tasks: [Task]
+    @State private var enteredString: String = ""
+        
+    var body: some View {
+        HStack {
+            TextField("Add New Task", text: $enteredString)
+            Button(action:{tasks.append(Task(nameOfTask: enteredString))}, label: {
+                Text("+")
+            })
+           
+        }
+    }
+}
